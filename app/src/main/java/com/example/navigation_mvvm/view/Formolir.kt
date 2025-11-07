5ea16ba57862df27308aa254732d2396fd4ec806
dannyfadli <danyfdly95@gmail.com>
2025-11-07 10:03:10 +0700

    Tambahkan struktur dasar Scaffold dan TopAppBar untuk layar Home

--- app/src/main/java/com/example/navigation_mvvm/view/Formolir.kt
+++ app/src/main/java/com/example/navigation_mvvm/view/Formolir.kt
@@ -1,10 +1,31 @@
 package com.example.navigation_mvvm.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.navigation_mvvm.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-Laki","Perempuan"),
-)
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier= Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_700))
+            )}
+    ){
+
+    }
+}
