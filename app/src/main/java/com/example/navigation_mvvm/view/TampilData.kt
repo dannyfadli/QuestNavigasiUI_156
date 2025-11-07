65f292c0f70758f68cc4faab8e7f5fb6adaccb0e
dannyfadli <danyfdly95@gmail.com>
2025-11-07 10:56:35 +0700

    Tambahkan struktur Scaffold dan TopAppBar untuk layar TampilData

--- app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
@@ -2,7 +2,14 @@ package com.example.navigation_mvvm.view
 
 import com.example.navigation_mvvm.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,4 +22,13 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin),"Lainya"),
         Pair(stringResource(id = R.string.alamat),"Yogyakarta")
     )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+            )
+        }){
+
+    }
 }
