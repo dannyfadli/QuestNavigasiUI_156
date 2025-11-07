cf5a4bcbf1abb84884be52be30c0003095bd9bea
dannyfadli <danyfdly95@gmail.com>
2025-11-07 10:50:33 +0700

    Buat composable TampilData untuk menampilkan ringkasan data pengguna

--- app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
@@ -1,2 +1,18 @@
 package com.example.navigation_mvvm.view
 
+import com.example.navigation_mvvm.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap),"Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin),"Lainya"),
+        Pair(stringResource(id = R.string.alamat),"Yogyakarta")
+    )
+}
