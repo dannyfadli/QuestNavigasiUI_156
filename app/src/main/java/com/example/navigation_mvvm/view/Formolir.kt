6eda64b4ebd8c2eccb80753445f667af006e68b2
dannyfadli <danyfdly95@gmail.com>
2025-11-07 10:13:38 +0700

    Tambahkan OutlinedTextField untuk input Nama Lengkap

--- app/src/main/java/com/example/navigation_mvvm/view/Formolir.kt
+++ app/src/main/java/com/example/navigation_mvvm/view/Formolir.kt
@@ -1,16 +1,22 @@
 package com.example.navigation_mvvm.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigation_mvvm.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,12 +33,30 @@ fun FormIsian(
                     colorResource(id = R.color.teal_700))
             )}
     ){ isiRuang ->
-        Colomn(modifier = Modifier.padding( isiRuang))
+        Colomn(modifier = Modifier.padding( isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier
+            )
+        }
 
     }
 }
 
 @Composable
-fun Colomn() {
+fun Colomn(
+    modifier: Modifier,
+    verticalArrangement: Arrangement.Vertical,
+    horizontalAlignment: Alignment.Horizontal
+) {
     TODO("Not yet implemented")
 }
