11814a247017b3daa54b8f6b4c8ad0a1442cfb52
dannyfadli <danyfdly95@gmail.com>
2025-11-07 11:11:30 +0700

    meneyelesaikan tampilanData

--- app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
+++ app/src/main/java/com/example/navigation_mvvm/view/TampilData.kt
@@ -2,9 +2,14 @@ package com.example.navigation_mvvm.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import com.example.navigation_mvvm.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,7 +20,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,7 +45,22 @@ fun TampilData(
         Column(modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.SpaceBetween){
             Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = 10.dp))){}
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
+                items.forEach { items->
+                    Column {
+                        Text(text = items.first.uppercase(), fontSize = 16.sp)
+                        Text(text = items.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id = R.string.back))
+                }
+                ) { }
+            }
         }
 
     }
